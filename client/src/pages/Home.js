aed95d4220dfe0e59b459dfc6e7e4a78816e0991
Timothy Barnaby <tbarnaby1@gmail.com>
2023-07-02 12:23:47 -0500

    added home page

---
+++ client/src/pages/Home.js
@@ -0,0 +1,14 @@
+
+import portrait from '../portrait.jpg';
+
+const Home = () => {
+  return (
+    <div>Home
+
+<img src={portrait} className="App-portrait" alt="portrait" />
+
+    </div>
+  )
+}
+
+export default Home
