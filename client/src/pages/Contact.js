c104cd704d1a8f4f885cf0c7b7925384291f6218
Timothy Barnaby <tbarnaby1@gmail.com>
2023-07-05 15:15:04 -0500

    update

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -2,16 +2,15 @@ import React from 'react'
 import './Contact.css'
 
 const Contact = () => {
-  return (
-    <div id='contactDiv' >
-        
-        <a href="mailto:tbarnaby1@gmail.com" className="EmailButton">
-  Contact Me
-</a>
-
-
-    </div>
-  )
+    return (
+        <div id='contactDiv' >
+            <a href="mailto:tbarnaby1@gmail.com" className="EmailButton">
+                <div id='emailContactDiv'>
+                    Contact Me
+                </div>
+            </a>
+        </div>
+    )
 }
 
 export default Contact
