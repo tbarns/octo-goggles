4389d5fd92302e0c9fc14e548ef50a2e49a59b78
Timothy Barnaby <tbarnaby1@gmail.com>
2023-07-05 16:50:07 -0500

    contact page

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -4,17 +4,21 @@ import './Contact.css'
 const Contact = () => {
     return (
         <div id='contactDiv' >
+
             <a href="mailto:tbarnaby1@gmail.com" className="EmailButton">
-                <div id='emailContactDiv'>
+                <div className='glow-on-hover-contact' id='emailContactDiv'>
                     email
                 </div>
             </a>
 
             <a href="https://docs.google.com/document/d/1uBKcYC079YrG2t9TqskE56-PqwLS_eqXlTi39cbSqZ8/edit?usp=sharing" className="resumeButton">
-                <div id='resumeContactDiv'>
+                <div className='glow-on-hover-contact' id='resumeContactDiv'>
                     resume
                 </div>
             </a>
+            <div className='glow-on-hover-contact' id='otherContactDiv'>
+                other
+            </div>
 
         </div>
 
