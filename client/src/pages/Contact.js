78d2a142e83ebc5b7b037fb5c251bacf4ad7b7d4
Timothy Barnaby <tbarnaby1@gmail.com>
2023-07-05 17:14:59 -0500

    update to contact

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -5,17 +5,21 @@ const Contact = () => {
     return (
         <div id='contactDiv' >
 
-            <a href="mailto:tbarnaby1@gmail.com" className="EmailButton">
-                <div className='glow-on-hover-contact' id='emailContactDiv'>
-                    email
-                </div>
-            </a>
 
-            <a href="https://docs.google.com/document/d/1uBKcYC079YrG2t9TqskE56-PqwLS_eqXlTi39cbSqZ8/edit?usp=sharing" className="resumeButton">
-                <div className='glow-on-hover-contact' id='resumeContactDiv'>
+            <div className='glow-on-hover-contact' id='emailContactDiv'>
+                <a href="mailto:tbarnaby1@gmail.com" className="EmailButton">
+                    email
+                </a>
+            </div>
+
+
+
+            <div className='glow-on-hover-contact' id='resumeContactDiv'>
+                <a href="https://docs.google.com/document/d/1uBKcYC079YrG2t9TqskE56-PqwLS_eqXlTi39cbSqZ8/edit?usp=sharing" className="resumeButton">
                     resume
-                </div>
-            </a>
+                </a>
+            </div>
+
             <div className='glow-on-hover-contact' id='otherContactDiv'>
                 other
             </div>
